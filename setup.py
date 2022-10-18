e14983028fd80253429b920cc50b771494e7406d
Marcelo Silveira Vilar <tchelovilar@gmail.com>
2022-10-18 18:19:36 +0200

    Update setup requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-import pathlib
-
 from setuptools import setup
 
-here = pathlib.Path(__file__).parent.resolve()
-
-install_requires = (here / 'kindplay.egg-info/requires.txt').read_text(encoding='utf-8').splitlines()
-
-setup(
-    install_requires=install_requires
-)
+setup()
